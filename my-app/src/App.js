1aefd064b786b04a3752af8f90aba4fe188ee771
lttungog <124686851+lttungog@users.noreply.github.com>
2023-11-17 11:24:08 +0700

    1711

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,31 +3,31 @@ import './App.css';
 // import UserList from './component/UserList';
 import ShoppingList from './component/ShoppingList';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default function MyApp() {
+function App() {
   return (
-    <div className = "App">
-      <ShoppingList />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+// export default function MyApp() {
+//   return (
+//     <div className = "App">
+//       <ShoppingList />
+//     </div>
+//   );
+// }
